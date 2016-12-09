6dfb0c1ea4fb3d12d14a07d0e831eb32f3b2f340
krasch <github@krasch.io>
2016-12-09 20:46:05 +0100

    Implement creating arguments for multiple strings

--- yaml_argparse.py
+++ yaml_argparse.py
@@ -7,10 +7,8 @@ def parse_arguments_based_on_yaml(yaml_file):
     with open(yaml_file) as f:
         yaml_data = yaml.load(f)
 
-    # to start with, support only a single parameter
-    key = list(yaml_data.keys())[0]
-    value = yaml_data[key]
     parser = argparse.ArgumentParser()
-    parser.add_argument("-{}".format(key), default=value)
+    for key, value in yaml_data.items():
+        parser.add_argument("-{}".format(key), default=value)
     args = parser.parse_args()
     return args
