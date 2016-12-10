d21cb8d66e8aed6dd3e75f288e401f8b31739ba5
krasch <github@krasch.io>
2016-12-10 17:40:26 +0100

    Nested parameters work

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import sys
 import yaml
 from nose.tools import assert_dict_equal, raises
 
-from yaml_argparse import parse_arguments_based_on_yaml, flatten_dict
+from yaml_argparse import parse_arguments_based_on_yaml, unflatten_dict, flatten_dict
 
 
 def create_yaml_and_parse_arguments(yaml_params, command_line_params):
@@ -15,6 +15,7 @@ def create_yaml_and_parse_arguments(yaml_params, command_line_params):
         config = parse_arguments_based_on_yaml(temp_file.name)
 
     return config
+
 
 @raises(SystemExit)
 def test_illegal_commmandline_param():
@@ -86,32 +87,99 @@ def test_multiple_strings_all_overwrite():
     assert_dict_equal(expected, actual)
 
 
-"""
 def test_nested_no_overwrite():
     yaml_params = {"key1": {"key1_1": "yaml_value_key1_1"}}
-    command_line_params = {}
+    command_line_params = []
     expected = {"key1": {"key1_1": "yaml_value_key1_1"}}
 
     actual = create_yaml_and_parse_arguments(yaml_params, command_line_params)
     assert_dict_equal(expected, actual)
-
 
 
 def test_nested_overwrite():
     yaml_params = {"key1": {"key1_1": "yaml_value_key1_1"}}
-    command_line_params = {"key1": {"key1_1": "cmd_value_key1_1"}}
-    expected = {"key1": {"key1_1": "yaml_value_key1_1"}}
+    command_line_params = ["-key1.key1_1=cmd_value_key1_1"]
+    expected = {"key1": {"key1_1": "cmd_value_key1_1"}}
 
     actual = create_yaml_and_parse_arguments(yaml_params, command_line_params)
     assert_dict_equal(expected, actual)
-"""
 
-"""
+
 def test_nested_overwrite_deep():
-    yaml_params = {"key1": {"subkey1": "yaml_value_subkey1"}}
-    command_line_params = {"key1": {"key1_1": {"key1_1_1": "cmd_value_key1_1_1","key1_1_2": "cmd_value_key1_1_2"}, "key1_2": "cmd_value_key1_2"}}
-    expected = {"key1": {"subkey1": "cmd_value_subkey1"}}
+    yaml_params = {"key1":
+                       {"key1_1":
+                            {"key1_1_1": "yaml_value_key1_1_1",
+                             "key1_1_2": "yaml_value_key1_1_2"},
+                        "key1_2": "yaml_value_key1_2"},
+                   "key2": "yaml_value_key2"}
+    command_line_params = ["-key1.key1_1.key1_1_2=cmd_value_key1_1_2", "-key2=cmd_value_key2"]
+    expected = {"key1":
+                    {"key1_1":
+                         {"key1_1_1": "yaml_value_key1_1_1",
+                          "key1_1_2": "cmd_value_key1_1_2"},
+                     "key1_2": "yaml_value_key1_2"},
+                "key2": "cmd_value_key2"}
 
     actual = create_yaml_and_parse_arguments(yaml_params, command_line_params)
     assert_dict_equal(expected, actual)
-"""
+
+
+
+def test_flatten_dict_empty():
+    dict_to_flatten = {}
+    expected = {}
+
+    actual = flatten_dict(dict_to_flatten)
+    assert_dict_equal(expected, actual)
+
+
+def test_flatten_dict_shallow():
+    dict_to_flatten = {"key1": "val1", "key2": "val2"}
+    expected = {("key1",): "val1", ("key2",): "val2"}
+
+    actual = flatten_dict(dict_to_flatten)
+    assert_dict_equal(expected, actual)
+
+
+def test_flatten_dict_nested():
+    dict_to_flatten = {"key1":
+                           {"key1_1": "val11",
+                            "key1_2":
+                                {"key1_2_1": "val121"}},
+                       "key2": "val2"}
+    expected = {("key1", "key1_1"): "val11",
+                ("key1", "key1_2", "key1_2_1"): "val121",
+                ("key2",): "val2"}
+
+    actual = flatten_dict(dict_to_flatten)
+    assert_dict_equal(expected, actual)
+
+
+def test_unflatten_dict_empty():
+    dict_to_unflatten = {}
+    expected = {}
+
+    actual = unflatten_dict(dict_to_unflatten)
+    assert_dict_equal(expected, actual)
+
+
+def test_unflatten_dict_shallow():
+    dict_to_unflatten = {("key1",): "val1", ("key2",): "val2"}
+    expected = {"key1": "val1", "key2": "val2"}
+
+    actual = unflatten_dict(dict_to_unflatten)
+    assert_dict_equal(expected, actual)
+
+
+def test_unflatten_dict_nested():
+    dict_to_unflatten = {("key1", "key1_1"): "val11",
+                         ("key1", "key1_2", "key1_2_1"): "val121",
+                         ("key2",): "val2"}
+    expected = {"key1":
+                    {"key1_1": "val11",
+                     "key1_2":
+                         {"key1_2_1": "val121"}},
+                "key2": "val2"}
+
+    actual = unflatten_dict(dict_to_unflatten)
+    assert_dict_equal(expected, actual)
